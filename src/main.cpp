9d768c7c5af4f5a21f0eed9d90e6166aa931bed2
Alexandre Carqueja <36893438+WALEX2000@users.noreply.github.com>
2018-10-22 16:21:55 +0100

    added main to src

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Proj1.cpp
+// Author      : Mafalda Santos, Diogo Silva, Alexandre Carqueja
+// Version     :
+// Copyright   : AEDA-2018
+// Description : College Management
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
