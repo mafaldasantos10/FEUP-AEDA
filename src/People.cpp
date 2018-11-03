a0a0fc01f87c1220bac7ce7e45cae7e2386a0378
Diogo Alex <38763303+diogoalex01@users.noreply.github.com>
2018-11-03 01:19:58 +0000

    Added some getters/setters and adders to these classes

--- src/People.cpp
+++ src/People.cpp
@@ -9,9 +9,9 @@
 
 //////PEOPLE//////
 
-People::People(string name, string adress, date& birthday, unsigned int phone, unsigned int cod) {
+People::People(string name, string address, date& birthday, unsigned int phone, unsigned int cod) {
 	this->name = name;
-	this->adress = adress;
+	this->address = address;
 	this->birthday = &birthday;
 	this->phone = phone;
 	this->cod = cod;
@@ -21,37 +21,133 @@ string People::getName(){
     return name;
 }
 
+void People::setName(string newName)
+{
+	name=newName;
+}
+
+string People::getAddress()
+{
+	return address;
+}
+
+void People::setAddress(string newAddress)
+{
+	address=newAddress;
+}
+
+int People::getPhone()
+{
+	return phone;
+}
+
+void People::setPhone(int newPhone)
+{
+	phone = newPhone;
+}
+
+int People::getCode()
+{
+	return cod;
+}
+
+void People::setCode(int newCod)
+{
+	cod=newCod;
+}
+
+date* People::getDate()
+{
+	return birthday;
+}
+
 void People::Show_Info(){
 
 }
 
 //////STUDENT//////
 
-Student::Student(string name, string adress, date birthday, unsigned int phone, unsigned int cod, Course *course)
-: People(name,adress,birthday,phone,cod){
+Student::Student(string name, string address, date birthday, unsigned int phone, unsigned int cod, Course *course)
+: People(name, address, birthday, phone, cod){
 	this->course = course;
+}
+
+Course* Student::getCourse()
+{
+	return course;
 }
 
 //////EMPLOYEE//////
 
-Employee::Employee(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif)
-: People(name,adress,birthday,phone,cod){
+Employee::Employee(string name, string address, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif)
+: People(name, address, birthday, phone, cod){
 	this->salary = salary;
 	this->nif = nif;
 }
 
+float Employee::getSalary()
+{
+	return salary;
+}
+
+void Employee::setSalary(float salary)
+{
+	this->salary = salary;
+}
+
+unsigned int Employee::getNIF()
+{
+	return nif;
+}
+
+void Employee::setNIF(unsigned int nif)
+{
+	this->nif = nif;
+}
+
+
 //////TEACHER//////
 
-Teacher::Teacher(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string category, vector<Uc *> subjects)
-: Employee(name, adress, birthday, phone, cod, salary, nif){
+Teacher::Teacher(string name, string address, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string category, vector<Uc *> subjects)
+: Employee(name, address, birthday, phone, cod, salary, nif){
 	this->category = category;
 	this->subjects = subjects;
 }
 
+string Teacher::getCategory()
+{
+	return category;
+}
+
+void Teacher::setCategory(string category)
+{
+	this->category = category;
+}
+
+vector<Uc*> Teacher::getSubjects()
+{
+	return subjects;
+}
+
+void Teacher::addSubject(Uc uc)
+{
+	subjects.push_back(&uc);
+}
+
 //////STAFF//////
 
-Staff::Staff(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string work_area)
-: Employee(name, adress, birthday, phone, cod, salary, nif){
+Staff::Staff(string name, string address, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string work_area)
+: Employee(name, address, birthday, phone, cod, salary, nif){
+	this->work_area = work_area;
+}
+
+string Staff::getWorkArea()
+{
+	return work_area;
+}
+
+void Staff::setWorkArea(string work_area)
+{
 	this->work_area = work_area;
 }
 
--- src/People.h
+++ src/People.h
@@ -12,6 +12,8 @@
 #include <map>
 #include <vector>
 
+//PROTOTYPES//
+//////////////////////
 class Course;
 class Uc;
 
@@ -23,14 +25,22 @@ struct date {  //formated date string for better use
 };
 
 class People {
-	string name, adress;
+	string name, address;
 	unsigned int phone, cod;
 	date* birthday;
 public:
-	People(string name, string adress, date& birthday, unsigned int phone, unsigned int cod);
+	People(string name, string address, date& birthday, unsigned int phone, unsigned int cod);
 	//virtual static void Build_Person(); //A ideia é usar isto como UI para pedir ao utilizador os parametros necessarios para construir um obj pessoa, ou seja pode ser feito sem objeto posterior
     virtual void Show_Info();
+    void setName(string newName);
     string getName();
+    string getAddress();
+    void setAddress(string newAddress);
+    int getPhone();
+    void setPhone(int newPhone);
+    int getCode();
+    void setCode(int newCod);
+    date* getDate();
 	virtual ~People();
 };
 
@@ -38,7 +48,8 @@ class Student : public People{
 	Course* course; //pointer to current course the student is enrolled in
 	map <Uc*, float> subjects; //In this map the Key is the subject and the float value corresponds to the grade of the student on that particular subject;
 public:
-	Student(string name, string adress, date birthday, unsigned int phone, unsigned int cod, Course *course);  //map is missing(will be resolved when deciding UI for constructors)
+	Student(string name, string address, date birthday, unsigned int phone, unsigned int cod, Course *course);  //map is missing(will be resolved when deciding UI for constructors)
+	Course* getCourse();
 };
 
 class Employee : public People{
@@ -46,6 +57,10 @@ class Employee : public People{
 	unsigned int nif;
 public:
 	Employee(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif);
+	float getSalary();
+	void setSalary(float salary);
+	unsigned int getNIF();
+	void setNIF(unsigned int nif);
 };
 
 class Teacher : public Employee{
@@ -53,12 +68,19 @@ class Teacher : public Employee{
 	vector <Uc*> subjects;  //subjects taught by the teacher
 public:
 	Teacher(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string category, vector <Uc *> subjects);
+	string getCategory();
+	void setCategory(string category);
+	vector<Uc*> getSubjects();
+
+	void addSubject(Uc uc); //for now it just adds a given UC
 };
 
 class Staff : public Employee{
 	string work_area;
 public:
 	Staff(string name, string adress, date birthday, unsigned int phone, unsigned int cod, float salary, unsigned int nif, string work_area);
+	string getWorkArea();
+	void setWorkArea(string work_area);
 };
 
 #endif /* SRC_PEOPLE_H_ */
